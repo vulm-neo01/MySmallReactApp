e9b55d11d6968ea8a93a8cc78137f1a90fbe3781
vulm-neo01 <minhvu8a@gmail.com>
2023-02-13 15:38:02 +0700

    Complete prj 1

--- 1-birthday-reminder/src/data.js
+++ 1-birthday-reminder/src/data.js
@@ -3,6 +3,7 @@ export default [
     id: 1,
     name: 'Bertie Yates',
     age: 29,
+    birthday: '01/06/2001',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
@@ -10,6 +11,7 @@ export default [
     id: 2,
     name: 'Hester Hogan',
     age: 32,
+    birthday: '21/08/2001',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
@@ -17,6 +19,7 @@ export default [
     id: 3,
     name: 'Larry Little',
     age: 36,
+    birthday: '24/07/2001',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
@@ -24,6 +27,7 @@ export default [
     id: 4,
     name: 'Sean Walsh',
     age: 34,
+    birthday: '15/12/2001',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
@@ -31,6 +35,7 @@ export default [
     id: 5,
     name: 'Lola Gardner',
     age: 29,
+    birthday: '15/06/2001',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
