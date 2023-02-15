dcd5c9d0b90c4bec62a47ecf5db263fc64eea48f
vulm-neo01 <minhvu8a@gmail.com>
2023-02-16 00:44:59 +0700

    Update prj2-Tours with content

--- 2-tours/src/Tours.js
+++ 2-tours/src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours}) => {
-  return <h2>tours component</h2>;
+  return (
+    <section>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
