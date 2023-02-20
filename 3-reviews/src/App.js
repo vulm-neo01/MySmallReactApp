def082d6295d5e21eb4a42042d036b0a08c2b065
vulm-neo01 <minhvu8a@gmail.com>
2023-02-21 00:00:23 +0700

    Create prj3 - Reviews and code setup

---
+++ 3-reviews/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>Reviews</h2>
+          <div className='underline'></div>
+          <Review />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 3-reviews/src/Review.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+  console.log(people[index]);
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'></img>
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='text'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight/>
+        </button>
+      </div>
+      <button className='random-btn'>
+        Surprise me!!!
+      </button>
+    </article>
+  );
+};
+
+export default Review;
